b8c69c8232920e8a0bce09610c75368e87ba5982
Teresa Gao <55665310+teresa-gao@users.noreply.github.com>
2026-03-28 14:10:24 -0400

    add waffle image carousel

---
+++ js/waffles.js
@@ -0,0 +1,12 @@
+const WAFFLE_IMAGES = [
+    'banana-pie.jpeg',
+    'blueberry-tarts.jpeg',
+    'cheese-night.jpeg',
+    'cookies.jpeg',
+    'dumplings.jpeg',
+    'mochi-donuts.jpeg',
+    'scallion-pancakes.jpeg',
+    'tangyuan.jpeg',
+];
+
+Object.freeze(WAFFLE_IMAGES);
